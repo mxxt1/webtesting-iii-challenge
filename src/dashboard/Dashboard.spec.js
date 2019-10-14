0c5071cc89e8e2ddda3b4444706f2d3be761baa9
mxxt1 <matt@dustcloudlabs.com>
2019-10-14 19:07:34 -0400

    issues with Control test for disabled button

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react'; 
+import { render, getByText } from '@testing-library/react'; 
 import Dashboard from './Dashboard';
+import Controls from '../controls/Controls';
+import Display from '../display/Display';
 // Test away
+
+//Dashboard component renders
+//Dashboard should render Controls and Display
 
 test('Dashboard component renders', () => {
     render(<Dashboard />);
 });
+
+test('Control component renders', () => {
+    render(<Controls />);
+});
+
+test('Display component renders', () => {
+    render(<Display />);
+});
+
+
+//defaults to 'unlocked' and 'open'
+
+test ('defaults to unlocked', () => {
+    const {getByText} = render(<Dashboard />);
+
+    getByText(/unlocked/i);
+    getByText(/open/i);
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -3,6 +3,31 @@ import { render } from '@testing-library/react';
 import Display from './Display';
 // Test away
 
+
+//Display component renders
 test('Display component renders', () => {
     render(<Display />);
 });
+
+
+//Displays if gate is open/closed
+//getByText Locked || Unlocked
+//test on 
+
+
+
+//Displays if gate is locked or unlocked
+//getByText Closed || Open
+
+
+//Displays closed if 'closed' prop = true
+
+
+
+//Displays locked if 'locked' prop = true
+
+
+// if 'locked' or 'closed, then 'red-led'
+
+
+//if 'unlocked' or 'open', then 'green-led
